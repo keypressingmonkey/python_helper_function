d93fe782b10b4251adeb1ed342a4c95f53eadbb2
Keypressingmonkey <malte-stoever@web.de>
2021-10-07 13:19:05 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='helperfunctions',
-    version='0.1.0',
+    version='0.0.10',
     author='KeypressingMonkey',
     author_email='keypressingmonkey@web.de',
     description='Commonly reused functions',
